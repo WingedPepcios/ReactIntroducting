dd1de6b5b63cec3dbd6127a0e5330ca68e54d3bf
Józef Puchałowicz <jpuchalowicz@gmail.com>
2020-06-24 23:56:50 +0200

    Dodanie inputów bez ulatwien dla Karolka

--- intro/src/data/literals.js
+++ intro/src/data/literals.js
@@ -1,3 +1,11 @@
 export default {
   back: 'Wróć',
+  wrongInputType: 'Nie podano typu inputa',
+  admin: 'Admin',
+  role: 'Rola',
+  avatarPlaceholder: 'Podaj link do avatara',
+  namePlaceholder: 'Podaj imię i nazwisko',
+  emailPlaceholder: 'Podaj adres e-mail',
+  errorEmail: 'Niepoprawny adres e-mail',
+  errorName: 'Nie podano imienia lub nazwiska',
 }
--- intro/src/layouts/Dashboard.jsx
+++ intro/src/layouts/Dashboard.jsx
@@ -3,6 +3,7 @@ import Header from '../modules/Header';
 import Content from '../modules/Content';
 import Footer from '../modules/Footer';
 import Image from '../atoms/Image';
+import Settings from '../modules/Settings';
 import { Josephe } from '../data/images';
 
 import '../styles.scss';
@@ -13,6 +14,7 @@ const Dashboard = () => {
       <Header>
         <Image src={Josephe.link} alt={Josephe.alt} />
       </Header>
+      <Settings />
       <Content />
       <Footer />
     </>
